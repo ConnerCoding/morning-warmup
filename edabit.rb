67d84ffe379fc3c6d49838638cf9ee9ada7601e2
Conner Schiller <conner.schiller@gmail.com>
2020-10-20 09:09:00 -0700

    exercise 10-20-20

--- edabit.rb
+++ edabit.rb
@@ -144,4 +144,52 @@ end
 ############
 ### TEST ###
 ############
-p sub_reddit("https://www.reddit.com/r/funny/")
+# p sub_reddit("https://www.reddit.com/r/funny/")
+
+################################################
+################## PROBLEM 5 ###################
+# https://edabit.com/challenge/jde4eK7h7GX6H5gQq
+# Printer Levels
+
+##############
+### PROMPT ###
+##############
+# Given a dictionary of how many more pages each ink color can print, output the maximum number of pages the printer can print before any of the colors run out.
+
+# Examples
+# ink_levels({
+#   "cyan": 23,
+#   "magenta": 12,
+#   "yellow": 10
+# }) ➞ 10
+
+# ink_levels({
+#   "cyan": 432,
+#   "magenta": 543,
+#   "yellow": 777
+# }) ➞ 432
+
+# ink_levels({
+#   "cyan": 700,
+#   "magenta": 700,
+#   "yellow": 0
+# }) ➞ 0
+# Notes
+# A single printed page requires each color once, so printing is not possible if any of the slots lack ink (see example #3).
+
+############
+### CODE ###
+############
+def ink_levels(inks)
+    inks.select { |key, val| val == inks.values.min }
+    inks.sort_by { |k, v| v }.first
+end
+
+############
+### TEST ###
+############
+p ink_levels({
+    "cyan": 432,
+    "magenta": 543,
+    "yellow": 777
+})
