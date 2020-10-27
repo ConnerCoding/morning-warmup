fbdd37dcecba966c6d52470a2fe2af5f1e16b8ab
Conner Schiller <conner.schiller@gmail.com>
2020-10-27 09:19:29 -0700

    exercise 10-27-20

--- edabit.rb
+++ edabit.rb
@@ -355,4 +355,37 @@ end
 ############
 ### TEST ###
 ############
-p percentage_changed("$1000", "$840")
+# p percentage_changed("$1000", "$840")
+
+################################################
+################## PROBLEM 11 ##################
+# https://edabit.com/challenge/k8tfLWa6dXpHssFKt
+# Applying Discounts
+
+##############
+### PROMPT ###
+##############
+# Create a function that applies a discount d to every number in the array.
+
+# Examples
+# get_discounts([2, 4, 6, 11], "50%") ➞ [1, 2, 3, 5.5]
+
+# get_discounts([10, 20, 40, 80], "75%") ➞ [7.5, 15, 30, 60]
+
+# get_discounts([100], "45%") ➞ [45]
+# Notes
+# The discount is the percentage of the original price (i.e the discount of "75%" to 12 would be 9 as opposed to taking off 75% (making 3)).
+# There won't be any awkward decimal numbers, only 0.5 to deal with.
+
+############
+### CODE ###
+############
+def get_discounts(nums, d)
+    percent = d.gsub(/%/, '').to_f / 100
+    nums.map { |num| num * percent }
+end
+
+############
+### TEST ###
+############
+p get_discounts([10, 20, 40, 80], "75%")
