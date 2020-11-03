5f5ff5d2e421ee8b1314fd90cb12aa3bfcb0ae52
Conner Schiller <conner.schiller@gmail.com>
2020-11-03 08:20:58 -0800

    exercise 11-3-20

--- edabit.rb
+++ edabit.rb
@@ -496,4 +496,38 @@ end
 ############
 ### TEST ###
 ############
-p same_ascii("AA", "B@")
+# p same_ascii("AA", "B@")
+
+################################################
+################## PROBLEM 15 ##################
+# https://edabit.com/challenge/a6b6ctKGtFHx7N59C
+# Return Only the Integer
+
+##############
+### PROMPT ###
+##############
+# Write a function that takes an array of elements and returns only the integers.
+
+# Examples
+# return_only_integer([9, 2, "space", "car", "lion", 16]) ➞ [9, 2, 16]
+
+# return_only_integer(["hello", 81, "basketball", 123, "fox"]) ➞ [81, 123]
+
+# return_only_integer([10, "121", 56, 20, "car", 3, "lion"]) ➞ [10, 56, 20, 3]
+
+# return_only_integer(["String",  true,  3.3,  1]) ➞ [1]
+# Notes
+# N/A
+
+############
+### CODE ###
+############
+def return_only_integer(arr)
+    arr.filter { |num| num.is_a? Integer }
+    # arr.grep(Integer)
+end
+
+############
+### TEST ###
+############
+p return_only_integer(["String",  true,  3.3,  1])
